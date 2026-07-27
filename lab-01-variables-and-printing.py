5b2c21fd6296e9195be82070ffd468c9d43cdf1a
Charles Kernel <charlesmuriuki152@gmail.com>
2026-07-27 14:15:35 +0300

    Add lab 1 solution

---
+++ lab-01-variables-and-printing.py
@@ -0,0 +1,21 @@
+# Lab 1: Variables and Printing
+
+name = "Charles"
+age = 22
+country = "Kenya"
+
+print("My name is", name)
+print("I am", age, "years old.")
+print("I live in", country)
+
+dream_career = "Cybersecurity Engineer"
+favorite_language = "Python"
+favorite_os = "Linux"
+
+print("Dream Career:", dream_career)
+print("Favorite Programming Language:", favorite_language)
+print("Favorite Operating System:", favorite_os)
+
+goal = "Become the best cybersecurity engineer in the world."
+
+print("Goal:", goal)
