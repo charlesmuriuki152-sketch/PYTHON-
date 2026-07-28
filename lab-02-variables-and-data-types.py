474ca4c072c108a1066c3a655480e97aef48e851
Charles Kernel <charlesmuriuki152@gmail.com>
2026-07-28 21:30:16 +0300

    Lab 2:variables and data types py

---
+++ lab-02-variables-and-data-types.py
@@ -0,0 +1,14 @@
+device = "smart phone"
+ip_address = "10.3.6.1"
+number_of_login_attempts = 5
+compatible = True
+
+print(device)
+print(ip_address)
+print(number_of_login_attempts)
+print(compatible)
+
+print(type(device))
+print(type(ip_address))
+print(type(number_of_login_attempts))
+print(type(compatible))
