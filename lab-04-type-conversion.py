a96960d25a63906f5af14ea68b403c43d6a2e1fc
Charles Kernel <charlesmuriuki152@gmail.com>
2026-07-29 00:15:37 +0300

    Add Lab 04 Python code

---
+++ lab-04-type-conversion.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+height = float(input("Enter your height (cm): "))
+cybersecurity = True
+
+print("My name is", name)
+print("I am", age, "years old, so next year I will be", age + 1, "years old.")
+print("My height now is", height, "cm, expecting to be about", height + 0.5, "cm a year from now.")
+print("Cybersecurity student:", cybersecurity)
+print(type(cybersecurity))
